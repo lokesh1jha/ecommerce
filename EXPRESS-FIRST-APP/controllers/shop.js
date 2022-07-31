83530f69714f32aa4349535f707074ad88835199
LOKESH KUMAR JHA <83881305+lokesh1jha@users.noreply.github.com>
2022-07-31 21:01:00 +0530

    Backend

--- EXPRESS-FIRST-APP/controllers/shop.js
+++ EXPRESS-FIRST-APP/controllers/shop.js
@@ -174,11 +174,12 @@ exports.getOrders = (req, res, next) => {
   req.user
     .getOrders({ include: ['products'] })
     .then(orders => {
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
-        orders: orders
-      });
+        res.json({ orders, sucess: true })
+      // res.render('shop/orders', {
+      //   path: '/orders',
+      //   pageTitle: 'Your Orders',
+      //   orders: orders
+      // });
     })
     .catch(err => console.log(err));
 };
